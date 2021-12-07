3dd5175b12066e54fdc909f7869e979be3324875
Raymond Gasper <raymond.j.gasper@gmail.com>
2021-12-07 10:02:44 -0500

    add some docstrings and run formatters

--- fastq2csv.py
+++ fastq2csv.py
@@ -1,15 +1,14 @@
 import argparse
-import logging
-from typing import List
-import re
 import dataclasses
 import os.path
+import re
 from itertools import zip_longest as izip_longest
+from typing import List
 
-def grouper(iterable, n:int, fillvalue=None):
+
+def grouper(iterable, n: int, fillvalue=None):
     args = [iter(iterable)] * n
     return izip_longest(*args, fillvalue=fillvalue)
-
 
 
 @dataclasses.dataclass()
@@ -28,30 +27,33 @@ class FastQRecord:
 
 
 def from_fastq_lines(lines: List[str]) -> FastQRecord:
-    lines = [l.strip() for l in lines] 
-    seq_id, desc = re.split(r'\s', lines[0], maxsplit=1)
+    lines = [line.strip() for line in lines]
+    seq_id, desc = re.split(r"\s", lines[0], maxsplit=1)
     seq = lines[1]
     # for some reason a line is a plus sign with no significance
     quality = lines[3]
-    return FastQRecord(seq_id=seq_id, description= desc, sequence=seq, quality=quality)
+    return FastQRecord(seq_id=seq_id, description=desc, sequence=seq, quality=quality)
 
 
 def fastq2csv(fastqfile, csvfile):
+    """ main work function, converts data from fq to csv format """
     csvfile.write(FastQRecord.csv_header())
-    
+
     for fastqreclines in grouper(fastqfile, 4):
         FQRecord = from_fastq_lines(fastqreclines)
         csvfile.write(FQRecord.to_csv())
 
+
 def get_csv_fname(fqfilepath: str) -> str:
-    return os.path.splitext(fqfilepath)[0] + '.csv'
+    return os.path.splitext(fqfilepath)[0] + ".csv"
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert FastQ file to CSV.")
     parser.add_argument("fqfile", type=str)
     args = parser.parse_args()
-    if '.fq' not in args.fqfile:
-        raise ValueError('must input a fastq file')
+    if ".fq" not in args.fqfile:
+        raise ValueError("must input a fastq file")
     outpath = get_csv_fname(args.fqfile)
-    with open(args.fqfile, 'r') as fqfile, open(outpath, 'w') as csvfile:
+    with open(args.fqfile, "r") as fqfile, open(outpath, "w") as csvfile:
         fastq2csv(fqfile, csvfile)
--- test_fastq2csv.py
+++ test_fastq2csv.py
@@ -1,18 +1,21 @@
 import unittest
+
 from fastq2csv import FastQRecord, from_fastq_lines, get_csv_fname, grouper
 
+
 class TestFastqParsing(unittest.TestCase):
+    """ unit tests for fastq record parsing """
+
     def test_lines_to_fqrecord_unix_linesep(self):
         seq_id = "@D00780:495:CDB21ANXX:6:1212:1167:1947"
         description = "1:N:0:2"
-        sequence = "ANTTTCGTTCATGGGTCATGTAGCTGCCTAAGCAGTTTGTATGCCCGCTTAAGTGGCCCTACTTTGGCTATCCTGGCTGAGGCGGTTGAAGATCGGAAGAG"
-        quality = "B#<BBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFFFFFFFFBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFBFFFFFFF</FF<F/FB<BFFF"
-        expected = FastQRecord(
-            seq_id=seq_id,
-            description=description,
-            sequence=sequence,
-            quality=quality,
+        sequence = (
+            "ANTTTCGTTCATGGGTCATGTAGCTGCCTAAGCAGTTTGTATGCCCGCTTAAGTGGCCCTACTTTGGCTATCCTGGCTGAGGCGGTTGAAGATCGGAAGAG"
         )
+        quality = (
+            "B#<BBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFFFFFFFFBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFBFFFFFFF</FF<F/FB<BFFF"
+        )
+        expected = FastQRecord(seq_id=seq_id, description=description, sequence=sequence, quality=quality)
 
         lines = [
             "@D00780:495:CDB21ANXX:6:1212:1167:1947 1:N:0:2\n",
@@ -26,14 +29,13 @@ class TestFastqParsing(unittest.TestCase):
     def test_lines_to_fqrecord_win_linesep(self):
         seq_id = "@D00780:495:CDB21ANXX:6:1212:1167:1947"
         description = "1:N:0:2"
-        sequence = "ANTTTCGTTCATGGGTCATGTAGCTGCCTAAGCAGTTTGTATGCCCGCTTAAGTGGCCCTACTTTGGCTATCCTGGCTGAGGCGGTTGAAGATCGGAAGAG"
-        quality = "B#<BBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFFFFFFFFBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFBFFFFFFF</FF<F/FB<BFFF"
-        expected = FastQRecord(
-            seq_id=seq_id,
-            description=description,
-            sequence=sequence,
-            quality=quality,
+        sequence = (
+            "ANTTTCGTTCATGGGTCATGTAGCTGCCTAAGCAGTTTGTATGCCCGCTTAAGTGGCCCTACTTTGGCTATCCTGGCTGAGGCGGTTGAAGATCGGAAGAG"
         )
+        quality = (
+            "B#<BBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFFFFFFFFBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFBFFFFFFF</FF<F/FB<BFFF"
+        )
+        expected = FastQRecord(seq_id=seq_id, description=description, sequence=sequence, quality=quality)
 
         lines = [
             "@D00780:495:CDB21ANXX:6:1212:1167:1947 1:N:0:2\r\n",
@@ -44,18 +46,16 @@ class TestFastqParsing(unittest.TestCase):
 
         self.assertEqual(expected, from_fastq_lines(lines))
 
-
     def test_lines_to_fqrecord_cleaned_lines(self):
         seq_id = "@D00780:495:CDB21ANXX:6:1212:1167:1947"
         description = "1:N:0:2"
-        sequence = "ANTTTCGTTCATGGGTCATGTAGCTGCCTAAGCAGTTTGTATGCCCGCTTAAGTGGCCCTACTTTGGCTATCCTGGCTGAGGCGGTTGAAGATCGGAAGAG"
-        quality = "B#<BBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFFFFFFFFBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFBFFFFFFF</FF<F/FB<BFFF"
-        expected = FastQRecord(
-            seq_id=seq_id,
-            description=description,
-            sequence=sequence,
-            quality=quality,
+        sequence = (
+            "ANTTTCGTTCATGGGTCATGTAGCTGCCTAAGCAGTTTGTATGCCCGCTTAAGTGGCCCTACTTTGGCTATCCTGGCTGAGGCGGTTGAAGATCGGAAGAG"
         )
+        quality = (
+            "B#<BBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFFFFFFFFBFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFBFFFFFFF</FF<F/FB<BFFF"
+        )
+        expected = FastQRecord(seq_id=seq_id, description=description, sequence=sequence, quality=quality)
 
         lines = [
             "@D00780:495:CDB21ANXX:6:1212:1167:1947 1:N:0:2",
@@ -66,40 +66,27 @@ class TestFastqParsing(unittest.TestCase):
 
         self.assertEqual(expected, from_fastq_lines(lines))
 
-
     def test_handles_quotes_in_seq_id(self):
         seq_id = 'abcd123"'
         description = "quotes"
         sequence = "ATCG"
         quality = "FFBB"
-        expected = FastQRecord(
-            seq_id=seq_id,
-            description=description,
-            sequence=sequence,
-            quality=quality,
-        )
+        expected = FastQRecord(seq_id=seq_id, description=description, sequence=sequence, quality=quality)
 
-        lines = [
-            'abcd123" quotes',
-            "ATCG",
-            "+",
-            "FFBB",
-        ]
+        lines = ['abcd123" quotes', "ATCG", "+", "FFBB"]
 
         self.assertEqual(expected, from_fastq_lines(lines))
 
     def test_grouper(self):
-        chunksize=4
+        chunksize = 4
         stuff = ["whatever"] * chunksize * 3
         for group in grouper(stuff, chunksize):
             self.assertEqual(len(group), chunksize)
-
 
     def test_filename_parsing_abspath(self):
         fqfile = "/Users/raymondgasper/Projects/work/fastq_playin/whatever.fq"
         csvfile = "/Users/raymondgasper/Projects/work/fastq_playin/whatever.csv"
         self.assertEqual(get_csv_fname(fqfile), csvfile)
-
 
     def test_filename_parsing_relpath(self):
         fqfile = "whatever.fq"
